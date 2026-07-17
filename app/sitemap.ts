1e241362465b2d4e703b6fa471a45f463bd9b16a
bharathkumarsaravanan <bharathsaravananofficial@gmail.com>
2026-07-17 19:00:53 +0530

    robot and sitemap added

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl ="https://contextdrop.vercel.app";
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: "weekly",
+            priority: 1
+        },
+        {
+            url: `${baseUrl}/demo`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8
+        },
+        {
+            url: `${baseUrl}/login`,
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.5
+        }
+    ]
+}

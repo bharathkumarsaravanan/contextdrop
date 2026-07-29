818f23d1efb3af97a22f749aaafbfb368efe8f9e
bharathkumarsaravanan <bharathsaravananofficial@gmail.com>
2026-07-29 21:17:16 +0530

    analytics installed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ['latin']
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Toaster richColors />
+        <Analytics />
       </body>
     </html>
   );
